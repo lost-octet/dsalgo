aecc6bf46bf2fe660a832beba860883453a82322
Lost Octet <lost.octet@gmail.com>
2022-01-01 19:54:32 +0530

    First Code V1.0

--- src/main/java/com/lostoctet/dsalgo/array/MaxConsecutiveOnes.java
+++ src/main/java/com/lostoctet/dsalgo/array/MaxConsecutiveOnes.java
@@ -1,8 +1,9 @@
 package com.lostoctet.dsalgo.array;
-
 import java.util.Scanner;
 
 public class MaxConsecutiveOnes {
+
+        //Brute force Approach
         public static int findMaxConsecutiveOnes(int[] nums) {
             int maxConsecutiveOnes=0;
             for (int i=0; i < nums.length; i++) {
@@ -20,6 +21,17 @@ public class MaxConsecutiveOnes {
             return maxConsecutiveOnes;
         }
 
+    //Another function solving the same problem in better memory and time complexity.
+    public static int findMaxConsecutiveOnes1(int[] nums) {
+        int maxConsecutiveOnes=0, currentCount=0;
+        for (int i:nums) {
+            if (i == 1) currentCount++;
+            else currentCount=0;
+            if(maxConsecutiveOnes < currentCount) maxConsecutiveOnes = currentCount;
+        }
+        return maxConsecutiveOnes;
+    }
+
     public static void main(String[] args) {
         Scanner scan=new Scanner(System.in);
         System.out.print("Enter the Size of Array: ");
@@ -28,6 +40,7 @@ public class MaxConsecutiveOnes {
         System.out.println("Enter the value of the Array Zero(0) and One(1) only:  ");
         for(int i = 0; i < len; i++) array[i]= scan.nextInt();
         System.out.println("Size of Maximum Consecutive 1 is= " + findMaxConsecutiveOnes(array));
+        System.out.println("Output using Other Function: " + findMaxConsecutiveOnes1(array));
 
     }
 }
